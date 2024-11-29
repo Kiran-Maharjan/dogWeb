32a3aa8240b6e158895178bf83d57862bb6fba95
kiran2248 <kiran.megatransact@gmail.com>
2024-11-29 16:16:35 +0545

    Details Form buildup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 export const routes: Routes = [];
+const routeConfig: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home page'
+    },
+     {
+        path: 'details',
+        component: DetailsComponent,
+        title: 'Details Page'
+    }
+    , {
+        path: 'details/:id',
+        component: DetailsComponent,
+        title: 'Details Page'
+    }
+]
+export default routeConfig;
