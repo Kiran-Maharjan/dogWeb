7dc92640666bfe12c909cfb1f6924e61721abbfa
kiran2248 <kiran.megatransact@gmail.com>
2024-11-29 16:16:35 +0545

    Details Form buildup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent,RouterModule],
+  // templateUrl: './app.component.html',
+  template:`
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+    </a>
+      <section class="content">
+        <!-- <app-home></app-home> -->
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  
+`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'dogWeb';
