0bdd8bc12b8323b47d71ee8121ed26dbb50408d8
kiran2248 <kiran.megatransact@gmail.com>
2024-12-02 18:42:27 +0545

    add dog to buy cart feature

---
+++ src/app/dog/dog.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dog',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './dog.component.html',
+  styleUrl: './dog.component.css'
+})
+export class DogComponent {
+  favoriteDogSpieces="";
+
+  //generate cart to add dog item
+  cart: String[] = [];
+  totalCost: number = 0;
+  currency:String = "USD";
+
+  addToCart(item:String):void{
+    console.log('Adding to cart:', this.favoriteDogSpieces);
+    // Add logic to save the favorite dog spieces to the cart
+    //add item to cart
+    this.cart.push( item);
+    //calculate total amount
+    this.totalCost += 10; // assuming each dog costs $10
+
+    alert( 'Added to cart:'+item);
+  }
+
+  checkout(){
+    alert('Checkout successful');
+  }
+}
