fe6b43cf2df03aac004dc7ab5ec7d75429cbd987
kiran2248 <kiran.megatransact@gmail.com>
2024-12-02 22:13:36 +0545

    user form built using reactive forms

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,12 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [NgOptimizedImage,FormsModule],
+  imports: [NgOptimizedImage, FormsModule, ReactiveFormsModule],
   template: `
         <!-- <img src="/assets/logo.svg" alt="Angular logo" width="320" height="320" /> --> 
         <!-- <img [src]="location_pin_url" alt="Angular logo" width="320" height="320" /> -->
@@ -16,24 +16,39 @@ import {FormsModule} from '@angular/forms';
   <img [ngSrc]="home_url" fill />
 </div> -->
   <h3>Fill the user details</h3>
-  <!-- <form> -->
-    <label>Name</label>
-    <input type="text" [(ngModel)]="name" placeholder="Enter your name"/>
-    <label>Email</label>
-    <input type="email" [(ngModel)]="email" placeholder="Enter your email"/>
-    <button type="submit">Submit</button>
-  <!-- </form>   -->
-    <p>Name: {{name}}</p>
-    <p>Email: {{email}}</p>
- 
+    <form [formGroup]="profileForm" (ngSubmit)="handleSubmit()">
+  <label>
+    Name
+    <input type="text" formControlName="name" />
+  </label>
+  <label>
+    Email
+    <input type="email" formControlName="email" />
+  </label>
+  <button type="submit">Submit</button>
+</form>
+
+-------------------------------------------------------
+<h3>Profile Form</h3>
+<p>Name: {{ profileForm.value.name }}</p>
+<p>Email: {{ profileForm.value.email }}</p> 
   `,
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-location_pin_url="/assets/location-pin.svg";
-home_url="https://images.pexels.com/photos/58997/pexels-photo-58997.jpeg?auto=compress&cs=tinysrgb&w=400";
-home_url_desc="this is the description";
+  location_pin_url = "/assets/location-pin.svg";
+  home_url="https://images.pexels.com/photos/58997/pexels-photo-58997.jpeg?auto=compress&cs=tinysrgb&w=320";
+  home_url_desc = "this is the description";
 
-name="";
-email="";
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+
+  handleSubmit() {
+    alert(
+      this.profileForm.value.name + ' | ' + this.profileForm.value.email
+    );
+  }
 }
